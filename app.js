e7e9fe3de34c2385b49c21e4f7b506a737d38f0c
Nehal Ahmed <nehal.ahmed@tallysolutions.com>
2025-04-17 19:38:00 +0530

    Switch to bcryptjs and remove native bcrypt

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const connectDB = require('./db/conn');
 
 const cors = require('cors');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const path = require('path');
 const fs = require('fs');
 const multer = require('multer');
